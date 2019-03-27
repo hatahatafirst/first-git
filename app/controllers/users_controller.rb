e713f0d47a3f472408ba4dcc47f6fbe31ec03ba8
Soichiro Hata <hatahataaaa@gmail.com>
2019-03-27 06:26:45 +0000

    admin4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :followings, :likes, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
   
   
   def index
