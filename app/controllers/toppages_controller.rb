a77ad339178e4621b63836b6d17126d984376483
Soichiro Hata <hatahataaaa@gmail.com>
2019-03-27 08:22:53 +0000

    paginate

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @hatapost = current_user.hataposts.build
-      @hataposts = current_user.feed_hataposts.order('created_at DESC').page(params[:page])
+      @hataposts = current_user.feed_hataposts.order('created_at DESC').page(params[:page]).per(5)
     end 
   end
 end
