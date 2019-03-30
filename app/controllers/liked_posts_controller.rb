efc0aeaec256da59472db4295abde0ba8eb66db0
Soichiro Hata <hatahataaaa@gmail.com>
2019-03-30 09:15:52 +0000

    design1

--- app/controllers/liked_posts_controller.rb
+++ app/controllers/liked_posts_controller.rb
@@ -9,7 +9,7 @@ class LikedPostsController < ApplicationController
   def destroy
     hatapost = Hatapost.find(params[:hatapost_id])
     current_user.unliked(hatapost)
-    flash[:success] = 'お気に入り登録を解除しました。'
+    flash[:danger] = 'お気に入り登録を解除しました。'
     redirect_to root_url
   end
 end
